ab66c01b6f4fcd67673e2acecc76bc2b960de640
alainlou <az2lou@uwaterloo.ca>
2019-11-25 22:17:25 -0500

    add FFT (Cooley-Tukey)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate lazy_static;
+
 extern crate num;
 
 use std::env;
@@ -6,9 +9,52 @@ use std::io::Read;
 
 use num::complex::Complex;
 
-const TAU: f64 = 6.283185307179586476925286766559005768394338798750211641949;
+lazy_static! {
+    static ref TAU: f64 = 6.283185307179586476925286766559005768394338798750211641949;
+    static ref J: Complex<f64> = Complex::i();
+}
 
-fn main() {
+fn dft(signal: Vec<Complex<f64>>) -> Vec<Complex<f64>> {
+    let length = signal.len();
+    let n = length as f64;
+    let mut fourier: Vec<Complex<f64>> = vec![Complex::new(0.0, 0.0); length];
+    for iter1 in 0..length {
+        for iter2 in 0..length {
+            let i = iter1 as f64;
+            let j = iter2 as f64;
+            
+            fourier[iter1] += signal[iter2] * (i*j*(*TAU)*(*J)/n).exp();
+        }
+    }
+    return fourier;
+}
+
+fn fft(signal: Vec<Complex<f64>>) -> Vec<Complex<f64>> {
+    let length = signal.len();
+    if length <= 1 {
+        return signal;
+    }
+
+    let n = length as f64;
+    
+    let mut even: Vec<Complex<f64>> = Vec::new();
+    even.extend(signal.iter().step_by(2));
+    even = fft(even);
+
+    let mut odd: Vec<Complex<f64>> = Vec::new();
+    odd.extend(signal.iter().skip(1).step_by(2));
+    odd = fft(odd);
+    
+    let mut fourier: Vec<Complex<f64>> = vec![Complex::new(0.0, 0.0); length];
+    for iter in 0..length/2 {
+        let i = iter as f64;
+        fourier[iter] += even[iter] + (-i*(*TAU)*(*J)/n).exp() * odd[iter];
+        fourier[iter+length/2] += even[iter] - (-i*(*TAU)*(*J)/n).exp() * odd[iter];
+    }
+    return fourier;
+}
+
+fn main() {  
     // Open file
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -21,30 +67,31 @@ fn main() {
     let mut text = String::new();    
     file.read_to_string(&mut text).expect("error reading file");
     let data = text.split(' ');
-    let mut signal: Vec<f64> = Vec::new();
+    let mut signal: Vec<Complex<f64>> = Vec::new();
     for s in data {
         signal.push(s.parse().unwrap());
     }
-    
-    // Compute result
-    let length = signal.len();
-    let n = length as f64;
-    let mut fourier: Vec<Complex<f64>> = Vec::with_capacity(length);
-    for _ in 0..length {
-        fourier.push(Complex::new(0.0, 0.0));
-    }
-    for iter1 in 0..length {
-        for iter2 in 0..length {
-            let i = iter1 as f64;
-            let j = iter2 as f64;
-            fourier[iter1].re += signal[iter2] * (TAU/n*i*j).cos();
-            fourier[iter1].im += -signal[iter2] * (TAU/n*i*j).sin();
-        }
+
+    // Store original signal
+    let original = signal.to_vec();
+
+    // Compute DFT result
+    let fourier1: Vec<Complex<f64>> = dft(signal);
+
+    // Print DFT result
+    println!("With DFT:");
+    let length = fourier1.len();
+    let scaling = length as f64 / 2.0;
+    for iter in 0..length/2 {
+        println!("{0} hz: {1:.10}", iter, fourier1[iter].norm()/scaling);
     }
 
-    // Print result
-    let scaling = length as f64 / 2.0;
-    for iter1 in 0..length/2 {
-        println!("{0} hz: {1:.10}", iter1, fourier[iter1].norm()/scaling);
+    // Compute FFT result
+    let fourier2: Vec<Complex<f64>> = fft(original);
+
+    // Print FFT result    
+    println!("With FFT:");
+    for iter in 0..length/2 {
+        println!("{0} hz: {1:.10}", iter, fourier2[iter].norm()/scaling);
     }
 }
